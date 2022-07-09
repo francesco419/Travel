d3e394bb875867c053b05c1de76c8dd0f6eed36c
Sanghean Lee <francesco419@naver.com>
2022-07-09 19:10:02 +0900

    1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div></div>
+      <div>
+        <p>공공데이터를 사용한 여행정보 사이트</p>
+      </div>
     </div>
   );
 }
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,6 +1,8 @@
 function MainPage(){
     return(
-        <div></div>
+        <div>
+            <p>hi there</p>
+        </div>
     )
 }
 
