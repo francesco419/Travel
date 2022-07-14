5a2da7003706316a87717c6a7d3282a43f73a9aa
Sanghean Lee <francesco419@naver.com>
2022-07-14 20:33:02 +0900

    1st

---
+++ src/Map.js
@@ -0,0 +1,12 @@
+function Map(){
+    return(
+        <div>
+            <p>hi there</p>
+            <p>외교부_국가∙지역별 여행경보 https://www.data.go.kr/iim/api/selectAPIAcountView.do</p>
+            <p>외교부_국가·지역별 일반사항 https://www.data.go.kr/data/15099534/openapi.do</p>
+            <p>외교부_국가·지역별 일반정보 https://www.data.go.kr/data/15076234/openapi.do</p>
+        </div>
+    )
+}
+
+export default Map;
