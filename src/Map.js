ae4717711b6bfd336874fa88df3a3c898a67259b
Sanghean Lee <francesco419@naver.com>
2022-07-18 19:51:49 +0900

    1

--- src/Map.js
+++ src/Map.js
@@ -5,7 +5,9 @@ function Map(){
             <p>외교부_국가∙지역별 여행경보 https://www.data.go.kr/iim/api/selectAPIAcountView.do</p>
             <p>외교부_국가·지역별 일반사항 https://www.data.go.kr/data/15099534/openapi.do</p>
             <p>외교부_국가·지역별 일반정보 https://www.data.go.kr/data/15076234/openapi.do</p>
-            <p>a</p>
+            <p>헤더설정 - 아이콘 / 대륙 / 국가 / 설정 /</p>
+            <p>국가별 중요 - 환율 / 여행경보 / 언어 / 핫스팟</p>
+            <p></p>
         </div>
     )
 }
