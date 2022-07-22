db13b1be6fac9b56bcc00ed85675e02a3fe1144d
Sanghean Lee <francesco419@naver.com>
2022-07-22 20:28:16 +0900

    1

--- src/Map.js
+++ src/Map.js
@@ -1,15 +1,6 @@
 function Map(){
     return(
         <div>
-            <p>hi there</p>
-            <p>외교부_국가∙지역별 여행경보 https://www.data.go.kr/iim/api/selectAPIAcountView.do</p>
-            <p>외교부_국가·지역별 일반사항 https://www.data.go.kr/data/15099534/openapi.do</p>
-            <p>외교부_국가·지역별 일반정보 https://www.data.go.kr/data/15076234/openapi.do</p>
-            <p>헤더설정 - 아이콘 / 대륙 / 국가 / 설정 /</p>
-            <p>국가별 중요 - 환율 / 여행경보 / 언어 / 핫스팟</p>
-            <p>각 나라별로 최소 3곳 스팟 정보 - 이름 / 위치 / 정보 / 랜드마크</p>
-            <p>전체이미지 - 대륙 - 나라 - 스팟</p>
-
             <ul>
                 <li>대륙
                     <ul>
@@ -33,6 +24,15 @@ function Map(){
                     </ul>
                 </li>
             </ul>
+            <p>hi there</p>
+            <p>외교부_국가∙지역별 여행경보 https://www.data.go.kr/iim/api/selectAPIAcountView.do</p>
+            <p>외교부_국가·지역별 일반사항 https://www.data.go.kr/data/15099534/openapi.do</p>
+            <p>외교부_국가·지역별 일반정보 https://www.data.go.kr/data/15076234/openapi.do</p>
+            <p>헤더설정 - 아이콘 / 대륙 / 국가 / 설정 /</p>
+            <p>국가별 중요 - 환율 / 여행경보 / 언어 / 핫스팟</p>
+            <p>각 나라별로 최소 3곳 스팟 정보 - 이름 / 위치 / 정보 / 랜드마크</p>
+            <p>전체이미지 - 대륙 - 나라 - 스팟</p>
+
         </div>
     )
 }
