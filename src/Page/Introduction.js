0ba8c9ce47dfa130e1c97951bfd2fd8a75f4eb75
Sanghean Lee <francesco419@naver.com>
2022-12-19 01:08:06 +0900

    2.3

--- src/Page/Introduction.js
+++ src/Page/Introduction.js
@@ -7,30 +7,35 @@ function Introduction(props){
     }
     return(
         <div className={styles.container}>
-            <div className={styles.intro1} style={{backgroundColor: "blue"}}>
-                <h1>여행정보</h1>
+            <div className={styles.intro1} style={{backgroundColor: "#802045"}}>
+                <div className={styles.intro_first}>
+                    <img className={styles.intro_first_img} src='https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png'/>
+                    <div className={styles.intro_first_text}>TravelAway</div>
+                </div>
+                <div className={styles.text}>
+                    <div>SCROLL</div>
+                    <div>∨</div>
+                    <div>∨</div>
+                </div>
             </div>
-            <div className={styles.intro1}  style={{backgroundColor: "red"}}>
-                <h1>소개</h1>
+            <div className={styles.intro1}  style={{backgroundColor: "#f8f0f2"}}>
+                <p>소개</p>
             </div>
-            <div className={styles.intro1}  style={{backgroundColor: "yellow"}}>
-                <h1>사용법1</h1>
+            <div className={styles.intro1}  style={{backgroundColor: "#802045"}}>
+                <p>사용법1</p>
             </div>
-            <div className={styles.intro1}  style={{backgroundColor: "orange"}}>
-                <h1>사용법2</h1>
-            </div>
-            <div className={styles.intro1}  style={{backgroundColor: "purple"}}>
-                <h1>사용법3</h1>
-            </div>
-            <div className={styles.intro1}  style={{backgroundColor: "green"}}>
-                <h1>감사 + 사용하러가기</h1>
-                <Link to={"/MainPage"}>메인페이지로-</Link>
+            <div className={styles.intro1}  style={{backgroundColor: "#f8f0f2"}}>
+                <p>감사 + 사용하러가기</p>
+                <Link to={`${process.env.PUBLIC_URL}/`} onClick={()=>{
+                sessionStorageSave();
+                props.setIntropage(false);
+                }}>메인페이지로
+                </Link>
             </div>
             <button className={styles.skip} onClick={()=>{
                 sessionStorageSave();
                 props.setIntropage(false);
-                }
-                }>SKIP
+                }}>SKIP
                 </button>
         </div>
     )
